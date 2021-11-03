88d7f0df515b3b687c9e4ad64854689a5dd5f7af
Maor Grushko <maorgru@gmail.com>
2021-11-03 09:09:45 +0200

    added cache

--- routes/shortening-routes.js
+++ routes/shortening-routes.js
@@ -1,12 +1,16 @@
+import { json } from "express";
 import { encode, decode } from "../shortening-logic/shortening-logic.js"
 
+
 export const initShorteningRoutes = (app) => {
-    app.post('/encode', (req, res) => {
-        let ans = { short: encode("test") };
+    app.post('/encode', json(), (req, res) => {
+        //add json validation
+        let ans = { shorturl: encode(req.body.url) };
         res.send(ans);
     });
-    app.post('/decode', (req, res) => {
-        let ans = { orig: decode("test") };
+    app.post('/decode', json(), (req, res) => {
+        //add json validation
+        let ans = { url: decode(req.body.shorturl) };
         res.send(ans);
     });
 }
--- shortening-logic/shortening-logic.js
+++ shortening-logic/shortening-logic.js
@@ -1,7 +1,14 @@
+import NodeCache from "node-cache";
+
+const shorteningCache = new NodeCache(/* in case we want to delete them after some time { stdTTL: 100, checkperiod: 120 }*/);
+
 export const encode = (url) => {
-    return url;
+    let encoded = `${url}short`;
+    shorteningCache.set(encoded, url);
+    return encoded;
 }
 
 export const decode = (url) => {
-    return url;
+    let decoded = shorteningCache.get(url);
+    return decoded;
 }
