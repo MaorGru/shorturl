a4340804cdf2d9f398448869b4f1d4b277e25527
Maor Grushko <maorgru@gmail.com>
2021-11-03 09:45:22 +0200

    added encode logic, updated tests

--- shortening-logic/shortening-logic.js
+++ shortening-logic/shortening-logic.js
@@ -1,9 +1,11 @@
 import NodeCache from "node-cache";
+import crypto from "crypto"
 
 const shorteningCache = new NodeCache(/* in case we want to delete them after some time { stdTTL: 100, checkperiod: 120 }*/);
 
 export const encode = (url) => {
-    let encoded = `${url}short`;
+    let hash = crypto.createHash('md5').update(url).digest('hex').substring(0, 6);
+    let encoded = `http://short.est/${hash}`;
     shorteningCache.set(encoded, url);
     return encoded;
 }
